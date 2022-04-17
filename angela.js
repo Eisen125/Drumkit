a2f770134bf7b7c02c94ad2dbee4cb3118fd135c
Eisen125 <93591649+Eisen125@users.noreply.github.com>
2022-04-17 13:11:03 +0300

    Add files via upload

---
+++ angela.js
@@ -0,0 +1,9 @@
+function houskeeper(yearofexp,name,Specilaize)
+{
+    this.yearofexp=yearofexp;
+    this.name=name;
+    this.Specilaize=specilaize;
+    this.clean = function clean(){
+        alert("your room is clean ");
+        }
+}
